b18b69672fb4ff7e8565ff6fd140e301a5cc749d
github username <github email address>
2019-03-20 15:21:09 +0000

    favicon gone

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express')
-const favicon = require('express-favicon')
 const path = require('path')
 const port = process.env.PORT || 8080
 const app = express()
 app.enable('trust proxy');
-app.use(favicon(__dirname + '/dist/icons/favicon.ico'))
 // the __dirname is the current directory from where the script is running
 app.use((req, res, next) => {
   if (req.protocol === 'https' || req.secure || req.headers.host.includes('localhost')) {
